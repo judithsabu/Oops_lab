a4cae0cec9dff18befea5242699aa2a42705bb73
judithsabu <101170580+judithsabu@users.noreply.github.com>
2022-03-31 15:37:01 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package add;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sjcet
+ */
+public class Add {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1,num2,sum;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter the first number");
+        num1=sc.nextInt();
+        System.out.println("enter the second number");
+        num2=sc.nextInt();
+        sc.close();
+        sum=num1+num2;
+        System.out.println("sum of these numbers:"+sum);
+   
+        
+        
+     
+        // TODO code application logic here
+    }
+    
+}
